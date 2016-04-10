8829ce809fa299dd4989ea7582a6775a89928f76
Peeter Kuusik <peeter6ik@gmail.com>
2016-04-10 16:37:42 +0300

    just del
    minor repair

--- plot1.R
+++ plot1.R
@@ -6,7 +6,6 @@ data<-read.table("household_power_consumption.txt",header=T, quote="\"", sep=";"
 data1=data
 # date modification
 data1$Date=as.Date(data1$Date,format="%d/%m/%Y")
-#data1$Time=strptime(data1$Time,format="%H:%M:%S")
 
 data1=subset(data1, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
 #evertything OK?
